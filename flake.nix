e48d7f53de89ea0d59b9351f5749b1d0ca90d12e
Felix Berger <felix.enok.berger@gmail.com>
2025-02-12 16:14:11 +0000

    Add Nix flake configuration and update .gitignore for direnv support

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "a devshell for editing my NixOS configs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell rec {
+          packages = with pkgs; [
+            alejandra
+          ];
+        };
+      }
+    );
+}
