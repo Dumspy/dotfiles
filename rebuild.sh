61f2fc8d74ee55de12306cc2da4adf7fac35dd29
Felix Berger <felix.enok.berger@gmail.com>
2025-02-13 15:16:38 +0000

    Add remote Docker host configuration to rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,9 @@
 declare -A systems
 systems=(
   ["wsl"]="nixos-rebuild switch --flake $HOME/dotfiles/nix#wsl"
-  ["docker-host"]="nixos-rebuild switch --flake $HOME/dotfiles/nix#docker-host"
   ["Felixs-MacBook-Air"]="nix-darwin switch --flake $HOME/dotfiles/nix#Felixs-MacBook-Air"
+  ["docker-host"]="nixos-rebuild switch --flake $HOME/dotfiles/nix#docker-host"
+  ["docker-host-remote"]="nixos-rebuild --use-remote-sudo --build-host nixos@192.168.1.202 --target-host nixos@192.168.1.202 switch --flake $HOME/dotfiles/nix#docker-host"
 )
 
 selected=$(printf "%s\n" "${!systems[@]}" | fzf --prompt="Select system to rebuild: ")
