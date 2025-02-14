287c75dfcae04ec756daba64c6f76c3e06521715
Felix Berger <felix.enok.berger@gmail.com>
2025-02-14 22:00:33 +0000

    Refactor Git configuration: remove old settings and centralize in new module

--- nix/hosts/darwin/home.nix
+++ nix/hosts/darwin/home.nix
@@ -26,4 +26,10 @@
         IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     '';
   };
+
+  programs.git = {
+    extraConfig = {
+      gpg.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+    };
+  };
 }
--- nix/hosts/wsl/home.nix
+++ nix/hosts/wsl/home.nix
@@ -23,24 +23,8 @@
   home.homeDirectory = "/home/nixos";
 
   programs.git = {
-    enable = true;
-
-    userName = "Felix Berger";
-    userEmail = "felix.enok.berger@gmail.com";
-
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHEA0GhkjkbuZGBnjtXSoQ9zpeXPCTTRYvfJX6RniI6";
-    };
-
     extraConfig = {
-      github.user = "dumspy";
-      core.excludesfile = "~/.gitignore_global";
-      core.hooksPath = "~/git-hooks";
-      core.sshCommand = "ssh.exe";
-      push.autoSetupRemote = true;
-      gpg.format = "ssh";
       gpg.ssh.program = "/mnt/c/Users/felix/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
-      commit.gpgSign = true;
     };
   };
 }
