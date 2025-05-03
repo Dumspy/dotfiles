cdbe4710e25f5393903a58669157138818533efb
Felix Berger <felix.enok.berger@gmail.com>
2025-05-03 22:02:30 +0000

    Refactor Caddy configuration: simplify virtual host creation and remove self-signed certificate settings

--- nix/hosts/docker-host/services/caddy/caddy.nix
+++ nix/hosts/docker-host/services/caddy/caddy.nix
@@ -3,78 +3,58 @@
   pkgs,
   lib,
   ...
-}: let
-  certloc = "/var/lib/acme/internal.rger.dev";
-
-  # Helper function for creating virtual hosts with similar configs
-  mkVirtualHost = {
-    name,
-    target,
-    isHttps ? false,
-  }: {
-    name = "${name}.internal.rger.dev";
-    value.extraConfig = ''
-      reverse_proxy ${
-        if isHttps
-        then "https"
-        else "http"
-      }://${target} ${
-        if isHttps
-        then ''
-          {
-            transport http {
-              tls_insecure_skip_verify
-            }
-          }
-        ''
-        else ""
-      }
-
-      tls ${certloc}/cert.pem ${certloc}/key.pem {
-        protocols tls1.3
-      }
-    '';
-  };
-in {
+}: {
   security.acme = {
     acceptTerms = true;
     defaults.email = "felix.enok.berger@gmail.com";
+    preliminarySelfsigned = false;
 
     certs."internal.rger.dev" = {
       group = config.services.caddy.group;
-      systemd.before = ["caddy.service"];
 
       domain = "internal.rger.dev";
       extraDomainNames = ["*.internal.rger.dev"];
       dnsProvider = "cloudflare";
       dnsPropagationCheck = true;
       environmentFile = config.sops.secrets."cloudflare/.env".path;
-      preliminarySelfsigned = false;  # Disable self-signed certificate
     };
   };
 
   services.caddy = {
     enable = true;
 
-    systemd.services.caddy.after = ["acme-internal.rger.dev.service"];
+    virtualHosts = let
+      sharedConfig = {
+        useACMEHost = "internal.rger.dev";
+      };
+    in {
+      router =
+        sharedConfig
+        // {
+          name = "router.internal.rger.dev";
+          value.extraConfig = ''
+            reverse_proxy https://192.168.1.1
+          '';
+        };
 
-    virtualHosts = builtins.listToAttrs [
-      (mkVirtualHost {
-        name = "router";
-        target = "192.168.1.1";
-        isHttps = true;
-      })
-      (mkVirtualHost {
-        name = "pve";
-        target = "192.168.1.200:8006";
-        isHttps = true;
-      })
-      (mkVirtualHost {
-        name = "ha";
-        target = "192.168.1.201:8123";
-        isHttps = false;
-      })
-    ];
+      pve =
+        sharedConfig
+        // {
+          name = "pve.internal.rger.dev";
+          value.extraConfig = ''
+            reverse_proxy https://192.168.1.200:8006
+          '';
+        };
+
+      ha =
+        sharedConfig
+        // {
+          name = "ha.internal.rger.dev";
+          value.extraConfig = ''
+            reverse_proxy http://192.168.1.201:8123
+          '';
+        };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
