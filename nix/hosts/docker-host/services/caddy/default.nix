3af0c983622e02744fefb4962e9bd1554ec92863
Felix Berger <felix.enok.berger@gmail.com>
2025-09-14 12:58:55 +0000

    Refactor Caddy service configuration to simplify virtual host setup and maintain TLS settings

--- nix/hosts/docker-host/services/caddy/default.nix
+++ nix/hosts/docker-host/services/caddy/default.nix
@@ -3,30 +3,20 @@
   pkgs,
   lib,
   ...
-}: let
-  certloc = "/var/lib/acme/rger.dev";
-  mkVirtualHost = {
-    name,
-    target,
-  }: {
-    name = "${name}.rger.dev";
-    value.extraConfig = ''
-      reverse_proxy https://${target} {
+}: {
+  services.caddy = {
+    enable = true;
+    virtualHosts."router.rger.dev".extraConfig = ''
+      reverse_proxy https://192.168.1.1 {
         transport http {
           tls_insecure_skip_verify
         }
       }
-      tls ${certloc}/cert.pem ${certloc}/key.pem {
+      tls /var/lib/acme/rger.dev/cert.pem /var/lib/acme/rger.dev/key.pem {
         protocols tls1.3
       }
     '';
-  };
-in {
-  services.caddy = {
-    enable = true;
-    virtualHosts."router.rger.dev".extraConfig = ''
-      reverse_proxy http://192.168.1.1
-    '';
+    automaticHttps = false;
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
