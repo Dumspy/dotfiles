64b9ac1e831936da83517ebba63848f368171a19
Felix Berger <felix.enok.berger@gmail.com>
2025-09-14 10:29:36 +0000

    Refactor ACME configuration by removing extra Lego flags and consolidating DNS resolver settings

--- nix/hosts/docker-host/services/acme/default.nix
+++ nix/hosts/docker-host/services/acme/default.nix
@@ -10,10 +10,9 @@
     defaults = {
       email = "admin@rger.dev";
       dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      dnsPropagationCheck=false;
       environmentFile = config.sops.secrets."cloudflare/.env".path;
-      extraLegoFlags = [
-        "--dns.resolvers=1.1.1.1:53,8.8.8.8:53"
-      ];
     };
 
     certs."rger.dev" = {
