4ddf138f784e62ab11a9001fa543ca626aa77a96
Felix Berger <felix.enok.berger@gmail.com>
2025-09-14 14:36:58 +0000

    Refactor Caddy configuration: update reverse proxy settings for pve.rger.dev

--- nix/hosts/docker-host/services/caddy/default.nix
+++ nix/hosts/docker-host/services/caddy/default.nix
@@ -6,8 +6,8 @@
 }: {
   services.caddy = {
     enable = true;
-    virtualHosts."argocd.rger.dev".extraConfig = ''
-      reverse_proxy http://192.168.1.202:30080
+    virtualHosts."pve.rger.dev".extraConfig = ''
+      reverse_proxy http://192.168.1.200:8006/
       tls /var/lib/acme/rger.dev/cert.pem /var/lib/acme/rger.dev/key.pem {
         protocols tls1.3
       }
