f391f179ba59fcd1a08f997113dcd213d9a1ce03
Felix Berger <felix.enok.berger@gmail.com>
2025-12-06 21:29:20 +0000

    Replace custom SSH agent relay with systemd 1Password agent service

--- nix/hosts/wsl-devbox/home.nix
+++ nix/hosts/wsl-devbox/home.nix
@@ -8,25 +8,8 @@
     ../home.nix
   ];
 
-  home.file = {
-    ".ssh_pipe".source = ../../../zsh/.ssh_pipe;
-  };
-
-  programs.zsh = {
-    initContent = ''
-      source ~/.ssh_pipe
-    '';
-  };
-
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "${me.username}";
   home.homeDirectory = "${me.homePrefix}";
-
-  programs.git = {
-    extraConfig = {
-      gpg.ssh.program = "/mnt/c/Users/felix/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
-      core.sshCommand = "ssh.exe";
-    };
-  };
 }
