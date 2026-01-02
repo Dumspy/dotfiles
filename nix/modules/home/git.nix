c94b4524499e97ecd52f412df63091ee9aa4c3fa
Felix Berger <felix.enok.berger@gmail.com>
2026-01-02 10:09:04 +0000

    Formatting Git, exportes

--- nix/modules/home/git.nix
+++ nix/modules/home/git.nix
@@ -6,14 +6,12 @@
   programs.git = {
     enable = true;
 
-    userName = "Felix Berger";
-    userEmail = "felix.enok.berger@gmail.com";
+    settings = {
+      user = {
+        name = "Felix Berger";
+        email = "felix.enok.berger@gmail.com";
+      };
 
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHEA0GhkjkbuZGBnjtXSoQ9zpeXPCTTRYvfJX6RniI6";
-    };
-
-    extraConfig = {
       github.user = "dumspy";
       core.excludesfile = "~/.gitignore_global";
       core.hooksPath = "~/git-hooks";
@@ -21,12 +19,15 @@
       gpg.format = "ssh";
       commit.gpgSign = true;
       core.editor = "nvim";
-    };
 
-    aliases = {
-      dlog = "-c diff.external=difft log --ext-diff";
-      dshow = "-c diff.external=difft show --ext-diff";
-      ddiff = "-c diff.external=difft diff";
+      aliases = {
+        dlog = "-c diff.external=difft log --ext-diff";
+        dshow = "-c diff.external=difft show --ext-diff";
+        ddiff = "-c diff.external=difft diff";
+      };
+    };
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHEA0GhkjkbuZGBnjtXSoQ9zpeXPCTTRYvfJX6RniI6";
     };
   };
 }
