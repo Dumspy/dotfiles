ae5dffbb7a4c747e9189e11704b3f6ded839f2a7
Felix Berger <felix.enok.berger@gmail.com>
2026-01-18 08:55:22 +0100

    Simplify host config logic and remove WSL-specific npiperelayPath

--- hosts/config.nix
+++ hosts/config.nix
@@ -3,40 +3,36 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHuBvk3U7Pdlf5vUV6eH1VvUDigRHDRMp+d+pdo7jTky main-key";
 
   hostConfig =
-    if pkgs.stdenv.isDarwin
-    then {
-      username = "felix.berger";
-      homePrefix = "/Users/felix.berger";
-      editor = "zed";
-    }
-    else if config.networking.hostName == "wsl-devbox"
-    then {
-      username = "nixos";
-      homePrefix = "/home/nixos";
-      editor = "nvim";
-      npiperelayPath = "/mnt/c/bin/npiperelay.exe";
-    }
-    else {
-      username = "nixos";
-      homePrefix = "/home/nixos";
-      editor = "nvim";
-    };
+    if pkgs.stdenv.isDarwin then
+      {
+        username = "felix.berger";
+        homePrefix = "/Users/felix.berger";
+        editor = "zed";
+      }
+    else
+      {
+        username = "nixos";
+        homePrefix = "/home/nixos";
+        editor = "nvim";
+      };
 
   globalConfig = {
     dotfiles = "${hostConfig.homePrefix}/dotfiles";
     publicKey = publicKey;
-    sshKeys = [publicKey];
+    sshKeys = [ publicKey ];
   };
-in {
+in
+{
   config.var = lib.recursiveUpdate globalConfig hostConfig;
 
   options.var = lib.mkOption {
     type = lib.types.attrs;
-    default = {};
+    default = { };
     description = "Global configuration variables available to all modules";
   };
 }
