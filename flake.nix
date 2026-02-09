df409249318296d140aa2e1bbc2b3d36510bbf5e
Felix Berger <felix.enok.berger@gmail.com>
2026-02-09 20:13:00 +0000

    Add hardware config and enable oci-node-2

--- flake.nix
+++ flake.nix
@@ -196,18 +196,18 @@
           ];
         };
 
-        # oci-node-2 = mkNixos {
-        #   name = "oci-node-2";
-        #   system = "aarch64-linux";
-        #   specialArgs = {
-        #     username = "nixos";
-        #     inherit inputs;
-        #   };
-        #   withHomeManager = false;
-        #   extraModules = [
-        #     opnix.nixosModules.default
-        #   ];
-        # };
+        oci-node-2 = mkNixos {
+          name = "oci-node-2";
+          system = "aarch64-linux";
+          specialArgs = {
+            username = "nixos";
+            inherit inputs;
+          };
+          withHomeManager = false;
+          extraModules = [
+            opnix.nixosModules.default
+          ];
+        };
 
         oci-node-3 = mkNixos {
           name = "oci-node-3";
